5e69b56a1e88a7fba5af7b067f9fa3de8ea4dfb0
AhmedEzz71090 <a.ezz@afaqy.com>
2023-06-14 21:43:40 +0300

    Get user data request

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -3,6 +3,7 @@ import users from '@app/users/users-list/Users.json';
 import { environment } from '@env/environment';
 import { Router } from '@angular/router';
 import { SharedService } from '@shared/services/shared.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users-list',
@@ -16,7 +17,7 @@ export class UsersListComponent implements OnInit {
   cols: any[] = [];
   filterCols: any[] = [];
   cid = '';
-  constructor(private router: Router, private sharedService: SharedService) {
+  constructor(private router: Router, private sharedService: SharedService, private http: HttpClient) {
     sharedService?.breadcrumb?.next([
       {
         label: 'Users',
@@ -28,6 +29,7 @@ export class UsersListComponent implements OnInit {
   ngOnInit() {
     this.cid = this.router.url;
     this.data = users;
+    this.getData();
     this.refactorData(this.data);
     setTimeout(() => {
       this.cols = [
@@ -39,6 +41,12 @@ export class UsersListComponent implements OnInit {
       ];
     }, 0);
     this.filterCols = ['fullName', 'userType', 'companyName'];
+  }
+
+  getData() {
+    this.http.get('/user?userId=64748fc3b209d207043d7128').subscribe((res: any) => {
+      console.log(res);
+    });
   }
 
   refactorData(data: any[]): any[] {

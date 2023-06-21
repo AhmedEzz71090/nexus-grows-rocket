5f492ae53070a1c56377629bbe1394aa32c6eace
ahmedezz <ahmedezz710907@gmail.com>
2023-06-21 14:19:54 +0300

    Create signup component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,8 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 import { LoginComponent } from './login.component';
+import { SignupComponent } from '@app/auth/signup/signup.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent, data: { title: marker('Login') } }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, data: { title: marker('Login') } },
+  { path: 'signup', component: SignupComponent, data: { title: marker('Signup') } },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
